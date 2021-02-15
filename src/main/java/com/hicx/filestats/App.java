28e76134fe192b26389999e3842d00d116c4d316
tomek0021 <trojek21@gmail.com>
2021-02-15 18:56:14 +0100

    Maven quickstart archetype init.

---
+++ src/main/java/com/hicx/filestats/App.java
@@ -0,0 +1,13 @@
+package com.hicx.filestats;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
